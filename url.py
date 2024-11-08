5d294b05018d36ecef1729dc5c1be2fed8fd19fd
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2024-11-08 09:56:51 +0700

    hhllo

---
+++ url.py
@@ -0,0 +1,33 @@
+domainPing = "https://ping.gtechdn.vn/ping"
+domainLogbantin = "https://ping.gtechdn.vn/logbantin"
+domainXacnhanketnoi = "https://ping.gtechdn.vn/xacnhanketnoi"
+domainXacnhanketnoilai = "https://ping.gtechdn.vn/xacnhanketnoi/xacnhanketnoilai"
+domainStartStream = "https://ping.gtechdn.vn/start-stream"
+domainMqtt = "mqtt.gtechdn.vn"
+portMqtt = 1883
+username_mqtt = 'mqtt'
+password_mqtt = 'adminmqtt'
+
+## local ##
+# domainPing = "http://192.168.1.2:8017/ping"
+# domainLogbantin = "http://192.168.1.2:8017/logbantin"
+# domainXacnhanketnoi = "http://192.168.1.2:8017/xacnhanketnoi"
+# domainXacnhanketnoilai = "http://192.168.1.2:8017/xacnhanketnoi/xacnhanketnoilai"
+# domainMqtt = "mqtt.gtechdn.vn"
+# portMqtt = 1883
+# username_mqtt = 'mqtt'
+# password_mqtt = 'adminmqtt'
+
+# tets
+# domainPing = "http://192.168.1.8:8017/ping"
+# domainLogbantin = "http://192.168.1.8:8017/logbantin"
+# domainXacnhanketnoi = "http://192.168.1.8:8017/xacnhanketnoi"
+# domainXacnhanketnoilai = "http://192.168.1.8:8017/xacnhanketnoi/xacnhanketnoilai"
+# domainUpdateMaxacthuc= "http://192.168.1.8:8017/ping/updatemaxacthuc"
+# domainKhoathietbi= "http://192.168.1.8:8017/ping/khoathietbi"
+# domainGhilog= "http://192.168.1.8:8017/ping/ghilog"
+# domainStartStream = "http://192.168.1.8:8017/start-stream"
+# domainMqtt = "mqtt.gtechdn.vn"
+# portMqtt = 1883
+# username_mqtt = 'mqtt'
+# password_mqtt = 'adminmqtt'
