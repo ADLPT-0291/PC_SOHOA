6f3c68939c494be9d06cb6c4499f011d84fd4265
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2024-11-08 16:02:53 +0700

    1

--- url.py
+++ url.py
@@ -13,6 +13,7 @@ domainPing = "http://192.168.1.2:8017/ping"
 domainLogbantin = "http://192.168.1.2:8017/logbantin"
 domainXacnhanketnoi = "http://192.168.1.2:8017/xacnhanketnoi"
 domainXacnhanketnoilai = "http://192.168.1.2:8017/xacnhanketnoi/xacnhanketnoilai"
+domainStartStream = "http://192.168.1.2:8017/start-stream"
 domainMqtt = "dcab58eb6e6a48f48c506685c17bbb52.s1.eu.hivemq.cloud"
 portMqtt = 8883
 username_mqtt = 'gtechdn'
