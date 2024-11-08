68c1700af7d0f5c8557f8df802080527533eb5c1
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2024-11-08 15:32:45 +0700

    Initial commit

--- url.py
+++ url.py
@@ -1,22 +1,23 @@
-domainPing = "https://ping.gtechdn.vn/ping"
-domainLogbantin = "https://ping.gtechdn.vn/logbantin"
-domainXacnhanketnoi = "https://ping.gtechdn.vn/xacnhanketnoi"
-domainXacnhanketnoilai = "https://ping.gtechdn.vn/xacnhanketnoi/xacnhanketnoilai"
-domainStartStream = "https://ping.gtechdn.vn/start-stream"
-domainMqtt = "mqtt.gtechdn.vn"
-portMqtt = 1883
-username_mqtt = 'mqtt'
-password_mqtt = 'adminmqtt'
-
-## local ##
-# domainPing = "http://192.168.1.2:8017/ping"
-# domainLogbantin = "http://192.168.1.2:8017/logbantin"
-# domainXacnhanketnoi = "http://192.168.1.2:8017/xacnhanketnoi"
-# domainXacnhanketnoilai = "http://192.168.1.2:8017/xacnhanketnoi/xacnhanketnoilai"
+# domainPing = "https://ping.gtechdn.vn/ping"
+# domainLogbantin = "https://ping.gtechdn.vn/logbantin"
+# domainXacnhanketnoi = "https://ping.gtechdn.vn/xacnhanketnoi"
+# domainXacnhanketnoilai = "https://ping.gtechdn.vn/xacnhanketnoi/xacnhanketnoilai"
+# domainStartStream = "https://ping.gtechdn.vn/start-stream"
 # domainMqtt = "mqtt.gtechdn.vn"
 # portMqtt = 1883
 # username_mqtt = 'mqtt'
 # password_mqtt = 'adminmqtt'
+
+# local ##
+domainPing = "http://192.168.1.2:8017/ping"
+domainLogbantin = "http://192.168.1.2:8017/logbantin"
+domainXacnhanketnoi = "http://192.168.1.2:8017/xacnhanketnoi"
+domainXacnhanketnoilai = "http://192.168.1.2:8017/xacnhanketnoi/xacnhanketnoilai"
+domainMqtt = "dcab58eb6e6a48f48c506685c17bbb52.s1.eu.hivemq.cloud"
+portMqtt = 8883
+username_mqtt = 'gtechdn'
+password_mqtt = 'gtechdn123'
+
 
 # tets
 # domainPing = "http://192.168.1.8:8017/ping"
